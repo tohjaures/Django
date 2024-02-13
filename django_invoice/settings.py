28dee3d72b2f1804a2c6ff08b3e55f6694587dfb
Toh lerou jaures <tohjaures1105@gmail.com>
2024-02-13 18:01:53 +0000

    feat:add new customer

--- django_invoice/settings.py
+++ django_invoice/settings.py
@@ -1,15 +1,26 @@
 
 from pathlib import Path
+
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+try:
+    from django.contrib.messages import  constants as messages
+    MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-info',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger'
+    }
+except Exception as e:
+    pass
+    
+
 SECRET_KEY = "django-insecure-bk--j3mum@lq$x=i1rc!y_bi$6qh5-1w&(o41og*zr1zcfe3p6"
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -108,9 +119,11 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-STATICFILES_DIRS = ( os.path.join(STATIC_URL, "static/" ),)
+STATICFILES_DIRS = [ 
+                    os.path.join(BASE_DIR, "static/" ),
+                    ]
 
-MEDIA_URL = "media/"
+MEDIA_URL = "/media/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 # Default primary key field type
--- fact_app/views.py
+++ fact_app/views.py
@@ -1,6 +1,63 @@
 from django.shortcuts import render
+from django.views import View
+from .models import *
+from django.contrib import messages
 
 # Create your views here.
 
-def home(request, *args, **kwargs):
-    return render(request, 'base.html')
+class HomeView(View):
+    """
+    Main view
+    """
+    templates_name = 'index.html'
+    
+    invoices = Invoice.objects.select_related('customer', 'save_by').all()
+    
+    context = {
+        'invoices': invoices
+    }
+    
+    def get(self, request, *args, **kwargs):
+        
+        return render(request, self.templates_name, self.context)
+    
+    def post(self, request, *args, **kwargs):
+        return render(request, self.templates_name, self.context)
+    
+class AddCustomerView(View):
+    """Add new customer"""
+    template_name = 'add_customer.html'
+    
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+    
+    def post(self, request, *args, **kwargs):
+        data = {
+            'name': request.POST.get('name'),
+            'email': request.POST.get('email'),
+            'phone': request.POST.get('phone'),
+            'address': request.POST.get('address'),
+            'sex': request.POST.get('sex'),
+            'age': request.POST.get('age'),
+            'city': request.POST.get('city'),
+            'zip_code': request.POST.get('zip'),
+            'save_by': request.user
+
+        }
+        
+        try:
+            created = Customer.objects.create(**data)
+
+            if created:
+
+                messages.success(request, "Customer registered successfully.")
+
+            else:
+
+                messages.error(request, "Sorry, please try again the sent data is corrupt.")
+
+        except Exception as e:    
+
+            messages.error(request, f"Sorry our system is detecting the following issues {e}.")
+            
+        return render(request, self.template_name)
